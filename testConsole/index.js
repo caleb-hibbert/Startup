dfaabc36ba9ece01fa7b6722eca92f8d61882cba
caleb-hibbert <calebhibbert07@gmail.com>
2025-02-25 23:03:33 -0700

    Prepped for React pt 1

--- testConsole/index.js
+++ testConsole/index.js
@@ -1,8 +1,14 @@
-function countdown() {
-  let i = 0;
-  while (i++ < 7) {
-    console.log(`Counting ... ${i}`);
+var varCount = 20;
+let letCount = 20;
+
+console.log('Initial - var: %d, let: %d', varCount, letCount);
+
+for (var varCount = 1; varCount < 2; varCount++) {
+  for (let letCount = 1; letCount < 2; letCount++) {
+    console.log('Loop - var: %d, let: %d', varCount, letCount);
   }
 }
 
-countdown();
+const h1El = document.querySelector('h1');
+h1El.textContent = `Result - var:${varCount}, let:${letCount}`;
+console.log('Final - var: %d, let: %d', varCount, letCount);

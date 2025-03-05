1b4ef809045f28d8448f659163a5c4b1a865ae2a
caleb-hibbert <calebhibbert07@gmail.com>
2025-03-04 19:46:54 -0700

    Login page working

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+        <div className="form-group">
+            <h2>Log in</h2>
+            <br/>
+            {/* <div> */}
+                <form method="get" action="chat.html">
+                <label for="username">Username:</label>
+                <input type="username" id="username" name="username"></input>
+            {/* </div> */}
+                <br/>
+                <label for="password:">Password:</label>
+                <input type="password" id="passwordbox" name="varPassword" />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <br/>
+                <p>(First time? Just enter the username and password you'd like to use going forward!)</p>
+            </form>
+            
+        </div>
     </main>
   );
 }

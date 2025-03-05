f1e347ec5bd26ab05c2e394f3db87255e60d84ac
caleb-hibbert <calebhibbert07@gmail.com>
2025-03-04 19:09:10 -0700

    Header added to app.jsx, working correctly when running in browser

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+  <div className="body bg-dark text-light">App will display here
+  
+  
+  <header>
+
+    <h2> Chatting with friends! {" "} 
+        <img src= "https://upload.wikimedia.org/wikipedia/commons/d/d1/Bubbles-alt-icon.png" style={{width: "50px", height: "50px"}}/>
+    </h2>
+
+        {/* <title>CWF Login</title>
+        <link rel="stylesheet" href="indexstyles.css" />
+        <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/d/d1/Bubbles-alt-icon.png"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> */}
+
+
+    </header>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </div>
+  );
 }

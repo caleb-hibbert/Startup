d564a2361833f8fcb9f3aa79646faf704675bac7
caleb-hibbert <calebhibbert07@gmail.com>
2025-03-04 20:45:14 -0700

    mostly finished

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import "./chatstyles.css";
 
 export function Chat() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>Chat displayed here</div>
-    </main>
+        <title>CWF Chat</title>
+            <p>Welcome back, Caleb!</p>
+            <h3> Talking to: Adam</h3>
+
+            <div id="friendlistplusbox">
+                <table id="friendlist">
+                <tr>
+                    <th>Friends</th>
+                    <th></th>
+                    <th>Active?</th>
+                </tr>
+                <tr><th>Adam</th> <th></th><th>Yes</th></tr>
+                <tr><th>Jill</th> <th></th><th>No</th></tr>
+                <tr><th>Maxwell</th> <th></th><th>Yes</th></tr>
+
+                </table>
+                <br/>
+                <form>
+                    <tr><th>Add Friend: </th><th><input type="text" id="friendname" name="friendname" placeholder="Enter Friend Name"></input></th></tr>
+                    <button type="submit" className="btn btn-primary">Send</button>
+                </form>
+            </div>
+            <div>
+                <img id="chatbox" src="Chat window example.png" style={{width: "500px", height:"250px"}}></img>
+            </div>
+            <br/>
+            <form id="messageboxandsend">
+                <input type="text" id="message" name="message" placeholder="Enter your message here"></input>
+                <button type="submit" className="btn btn-primary">Send</button>
+            </form>
+
+            <ul id="jokebox">
+                <p>Joke of the day:</p>
+                <li>Knock Knock:</li>
+                <li>Who's there?</li>
+                <li>Tank</li>
+                <li>Tank who?</li>
+                <li>You're welcome!</li>
+            </ul>
+            <nav id="returntologinpage">
+                {/* <form method="get" action="index.html">
+                    <label for="returntologinpage:">Return to login page?</label>
+                    <button type="submit" className="btn btn-primary">Log out</button>
+                </form> */}
+            </nav>
+            
+        </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
+        <title>CWF Login</title>
         <div className="form-group">
             <h2>Log in</h2>
             <br/>
-            {/* <div> */}
+            
                 <form method="get" action="chat.html">
                 <label for="username">Username:</label>
                 <input type="username" id="username" name="username"></input>
-            {/* </div> */}
+            
                 <br/>
+                
                 <label for="password:">Password:</label>
                 <input type="password" id="passwordbox" name="varPassword" />
                 <button type="submit" className="btn btn-primary">Submit</button>

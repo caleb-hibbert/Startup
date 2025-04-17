8fd22c7362d8667988d9b8aaff92cb51a5579f40
caleb-hibbert <calebhibbert07@gmail.com>
2025-04-16 18:37:30 -0600

    Hooking up functions to DB

--- service/index.js
+++ service/index.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
+
+
 
 
 const authCookieName = 'token';
